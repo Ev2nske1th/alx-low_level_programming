6fe2726f636ca7390ef57c2167697ffd4cc05237
Ev2nske1th <otwane19@gmail.com>
2023-04-17 23:54:19 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_binary - converts unsigned int to binary
+ * print_binary - unsigned int to binary
  * @n: unsigned int
+ *
  * Return: binary
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
+
 		/*right shift operator*/
-		/*shifts all bits towards right by certain numbf 1 bits*/
+		/*shifts all bits towards right*/
+
 		print_binary(n >> 1);
 
 	_putchar((n & 1) + '0');
